ebb5251a6d67978ff77a469a9fac7379d6de5b84
Clementine00 <93353231+Clementine00@users.noreply.github.com>
2025-09-23 22:15:06 -0600

    added colour periwinkle

--- willow/app/src/main/java/com/example/willow/Shape.java
+++ willow/app/src/main/java/com/example/willow/Shape.java
@@ -2,10 +2,8 @@ package com.example.willow;
 
 public abstract class Shape {
 
-    String color:"periwinkle";
-
     int x;
     int y;
 
-    String color = "Gold";
+    String color = "Periwinkle";
 }
